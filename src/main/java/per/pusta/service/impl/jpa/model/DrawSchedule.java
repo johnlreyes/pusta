7e9e5efaaa769b3ac26fb39f23b863c06752d01d
John Reyes <JohnLReyes@yahoo.com>
2011-08-03 13:12:02 +0800

    added jpa model

--- src/main/java/per/pusta/service/impl/jpa/model/DrawSchedule.java
+++ src/main/java/per/pusta/service/impl/jpa/model/DrawSchedule.java
@@ -11,7 +11,17 @@ public class DrawSchedule implements Serializable {
     @SequenceGenerator(name = "draw_schedule_generator", sequenceName = "seq_draw_schedule", allocationSize = 1, initialValue = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "draw_schedule_generator")
 	private Integer id;
+    @OneToOne
+    @JoinColumn(name = "game_type_id")
 	private GameType gameType;
+    private Boolean sun;
+    private Boolean mon;
+    private Boolean tue;
+    private Boolean wed;
+    private Boolean thu;
+    private Boolean fri;
+    private Boolean sat;
+    private Integer hour;
 	
 	public Integer getId() {
 		return id;
@@ -20,4 +30,75 @@ public class DrawSchedule implements Serializable {
 		this.id = id;
 	}
 
+    public GameType getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(GameType gameType) {
+        this.gameType = gameType;
+    }
+
+    public Boolean getSun() {
+        return sun;
+    }
+
+    public void setSun(Boolean sun) {
+        this.sun = sun;
+    }
+
+    public Boolean getMon() {
+        return mon;
+    }
+
+    public void setMon(Boolean mon) {
+        this.mon = mon;
+    }
+
+    public Boolean getTue() {
+        return tue;
+    }
+
+    public void setTue(Boolean tue) {
+        this.tue = tue;
+    }
+
+    public Boolean getWed() {
+        return wed;
+    }
+
+    public void setWed(Boolean wed) {
+        this.wed = wed;
+    }
+
+    public Boolean getThu() {
+        return thu;
+    }
+
+    public void setThu(Boolean thu) {
+        this.thu = thu;
+    }
+
+    public Boolean getFri() {
+        return fri;
+    }
+
+    public void setFri(Boolean fri) {
+        this.fri = fri;
+    }
+
+    public Boolean getSat() {
+        return sat;
+    }
+
+    public void setSat(Boolean sat) {
+        this.sat = sat;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
 }
